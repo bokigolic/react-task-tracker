b6ea878c7f8ac19cb81f94dd69007d3b466d4d23
Bojan Golic <bokigolic32@gmail.com>
2022-02-27 00:00:42 -0500

    add component butto, add style through Header comppnent and basics CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = (props) => {
+const Header = ({title}) => {
   return (
-    <div>Task Tracker</div>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text="Hello" />
+
+    </header>
   )
 }
+
+Header.defaultProps = {
+  title:"Task Tracker"
+}
+
+Header.propTypes ={
+  title: PropTypes.string.isRequired
+}
+/*//css in JS
+const headingStyle = {
+  color: "red", backgroundColor:"black"
+
+}*/
 
 export default Header
